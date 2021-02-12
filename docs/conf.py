6c9f4addc187c4097ca1ee05db5b80a306695943
Tom Wagg <twagg@college.harvard.edu>
2021-02-12 12:26:50 -0500

    add extra extensions to make toc look better and other stuff

--- docs/conf.py
+++ docs/conf.py
@@ -31,14 +31,19 @@ author = 'Tom Wagg, Katie Breivik'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.todo",
+    "matplotlib.sphinxext.plot_directive",
+    "nbsphinx",
     'sphinx_automodapi.automodapi',
-    'matplotlib.sphinxext.plot_directive',
     'IPython.sphinxext.ipython_console_highlighting',
     'IPython.sphinxext.ipython_directive',
     'numpydoc',
     'sphinx_rtd_theme', 
-    'nbsphinx',
 ]
 
 # fix numpydoc autosummary
