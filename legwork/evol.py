c5e36e3bd1f6ac9a6773b0ddc53e564f4fd85b77
Tom Wagg <twagg@college.harvard.edu>
2021-02-19 14:49:29 -0500

    add funcs to all

--- legwork/evol.py
+++ legwork/evol.py
@@ -9,7 +9,8 @@ import astropy.units as u
 import astropy.constants as c
 
 __all__ = ['de_dt', 'evol_circ', 'evol_ecc',
-           'get_t_merge_circ', 'get_t_merge_ecc', 'evolve_f_orb_circ']
+           'get_t_merge_circ', 'get_t_merge_ecc', 'evolve_f_orb_circ',
+           'check_mass_freq_input', 'create_timesteps_array',]
 
 
 @jit
