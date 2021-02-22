01ca5ed882932206a932ec358c1a010a3f25668b
Tom Wagg <twagg@college.harvard.edu>
2021-02-21 21:09:48 -0500

    quickstart button wasn't getting linked

--- docs/_static/custom.js
+++ docs/_static/custom.js
@@ -1,11 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     // add links to nav boxes
-    boxes = document.querySelectorAll(".toms-nav-container .box");
-    console.log(boxes)
+    boxes = document.querySelectorAll(".toms-nav-container .box, .toms-nav-box");
     boxes.forEach(element => {
-        console.log(element);
         element.addEventListener("click", function() {
-            console.log(this);
             window.location.href = this.getAttribute("data-href");
         })
     });
