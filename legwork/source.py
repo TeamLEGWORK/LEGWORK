586aae90aa32b376cf896d631c0993a366921309
Tom Wagg <twagg@college.harvard.edu>
2021-11-16 14:00:58 -0800

    use evol not utils in source now

--- legwork/source.py
+++ legwork/source.py
@@ -390,11 +390,8 @@ class Source():
         if stationary is None:
             stat_mask = np.repeat(True, self.n_sources)
         elif stationary is True or stationary is False:
-            stat_mask = utils.determine_stationarity(m_c=self.m_c,
-                                                     f_orb_i=self.f_orb,
-                                                     t_evol=t_obs,
-                                                     ecc_i=self.ecc,
-                                                     stat_tol=self.stat_tol)
+            stat_mask = evol.determine_stationarity(m_c=self.m_c,f_orb_i=self.f_orb, t_evol=t_obs,
+                                                    ecc_i=self.ecc, stat_tol=self.stat_tol)
             if stationary is False:
                 stat_mask = np.logical_not(stat_mask)
         else:
