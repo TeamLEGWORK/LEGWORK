a3423ebb22e578aad524bd92d255f9b3847f9318
Tom Wagg <tomjwagg@gmail.com>
2026-07-09 12:51:23 -0400

    add helpers for regenerating peters_g data

---
+++ helpers/generate_g_data.py
@@ -0,0 +1,11 @@
+import numpy as np
+import legwork as lw
+
+n_range = np.arange(1, 10000 + 1).astype(int)
+e_range = np.linspace(0, 1, 1000)
+
+N, E = np.meshgrid(n_range, e_range)
+
+g_vals = lw.utils.peters_g(N, E)
+
+np.save("../legwork/peters_g.npy", g_vals)
